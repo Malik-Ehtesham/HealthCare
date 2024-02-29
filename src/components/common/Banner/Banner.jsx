61962dea4c558ab43854140f55505279345b7096
ehteshamzahid <ehteshamzahid313@gmail.com>
2024-03-01 00:37:47 +0500

    first commit

--- src/components/common/Banner/Banner.jsx
+++ src/components/common/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="w-screen bg-black py-1 flex justify-evenly fixed z-50 top-0 px-1">
       <div>
-        <p className="text-white text-center">03080554483</p>
+        {/* <p className="text-white text-center">03080554483</p> */}
         <p className="text-white   text-sm my-1 font-medium  tracking-widest">
           Wir sind rund um die Uhr verfügbar
         </p>
